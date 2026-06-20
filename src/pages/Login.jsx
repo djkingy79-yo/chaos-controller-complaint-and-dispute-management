6413850e401010abdcbdac0965eab118038e7770
Deb King <djkingy79@gmail.com>
2026-06-20 02:17:31 +0000

    File changes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -32,8 +32,8 @@ export default function Login() {
     base44.auth.loginWithProvider("google", "/dashboard");
   };
 
-  const handleFacebook = () => {
-    base44.auth.loginWithProvider("facebook", "/dashboard");
+  const handleMicrosoft = () => {
+    base44.auth.loginWithProvider("microsoft", "/dashboard");
   };
 
   return (
@@ -62,17 +62,7 @@ export default function Login() {
         <Button
           variant="outline"
           className="w-full h-12 text-sm font-medium"
-          onClick={handleFacebook}
-        >
-          <svg className="w-5 h-5 mr-2" viewBox="0 0 24 24" fill="#1877F2" xmlns="http://www.w3.org/2000/svg">
-            <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"/>
-          </svg>
-          Continue with Facebook
-        </Button>
-        <Button
-          variant="outline"
-          className="w-full h-12 text-sm font-medium"
-          onClick={() => base44.auth.loginWithProvider("microsoft", "/dashboard")}
+          onClick={handleMicrosoft}
         >
           <svg className="w-5 h-5 mr-2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path fill="#F25022" d="M0 0h11.5v11.5H0z"/>
@@ -81,16 +71,6 @@ export default function Login() {
             <path fill="#FFB900" d="M12.5 12.5H24V24H12.5z"/>
           </svg>
           Continue with Microsoft
-        </Button>
-        <Button
-          variant="outline"
-          className="w-full h-12 text-sm font-medium"
-          onClick={() => base44.auth.loginWithProvider("apple", "/dashboard")}
-        >
-          <svg className="w-5 h-5 mr-2" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-            <path d="M17.05 20.28c-.98.95-2.05.8-3.08.35-1.09-.46-2.09-.48-3.24 0-1.44.62-2.2.44-3.06-.35C2.79 15.25 3.51 7.7 9.05 7.31c1.35.07 2.29.74 3.08.8 1.18-.24 2.31-.93 3.57-.84 1.51.12 2.65.72 3.4 1.8-3.12 1.87-2.38 5.98.48 7.13-.57 1.56-1.32 3.1-2.54 4.08zm-3.1-17.43c.08 1.77-1.31 3.24-2.99 3.36-1.77.13-3.27-1.42-3.13-3.2C8.97 1.22 10.48-.03 12.11.01c.03 1.45.52 2.58.94 2.84z"/>
-          </svg>
-          Continue with Apple
         </Button>
       </div>
 
